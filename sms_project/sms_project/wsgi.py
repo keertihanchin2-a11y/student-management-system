3ffe0443b60fe2af46771faa101d2fb831d81019
Keerti Hanchin <keertihanchin2@gmail.com>
2026-04-14 12:46:44 +0700

    Fixed settings module path correctly

--- sms_project/sms_project/wsgi.py
+++ sms_project/sms_project/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sms_project.sms_project.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'sms_project.sms_project.settings'
+)
 
 application = get_wsgi_application()
