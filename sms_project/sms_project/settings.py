4a13b1e3047d7dee60002544582eafb95907eb77
Keerti Hanchin <keertihanchin2@gmail.com>
2026-04-14 12:33:10 +0700

    Final settings fix

--- sms_project/sms_project/settings.py
+++ sms_project/sms_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a2&6=hltx+97hhys7&w7%2+mw6bn^uc4+y@lm+v+kpmsn^c0a2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -116,5 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = ['static']
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
